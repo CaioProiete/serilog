afecfffef032742ea31a4f311464d6bf530b3c78
Caio Proiete <caio.proiete@gmail.com>
2016-11-09 13:16:47 -0800

    Add test to show dependencies are not copied

---
+++ src/MyLoggerConfig/Config.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace MyLoggerConfig
+{
+    public static class LoggerConfig
+    {
+        public static ILogger ConfigureLogging()
+        {
+            return new LoggerConfiguration()
+                .WriteTo.Seq("https://seq.mycompany.com")
+                .CreateLogger();
+        }
+    }
+}
---
+++ src/TestTransitiveDependencies/Program.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+namespace TestTransitiveDependencies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var log = Log.Logger = MyLoggerConfig.LoggerConfig.ConfigureLogging();
+                log.Information("Hello");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
